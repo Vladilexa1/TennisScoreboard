964d25eba21450105bac5ddfcf6b58b96a85d640
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-03-22 12:05:31 +0200

    Add: add model

---
+++ TennisScoreboard/Models/Match.cs
@@ -0,0 +1,12 @@
+﻿namespace TennisScoreboard.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public int Player1Id { get; set; }
+        public Player? Player1 { get; set; }
+        public int Player2Id { get; set; }
+        public Player? Player2 { get; set; }
+        public int Winner { get; set; }
+    }
+}
---
+++ TennisScoreboard/Models/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace TennisScoreboard.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
