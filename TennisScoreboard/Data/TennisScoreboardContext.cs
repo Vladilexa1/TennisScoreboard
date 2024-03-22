ad580dfa544ff9c84e3f97c490350e1643356b2d
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-03-22 12:13:05 +0200

    Add: Create DbContext

---
+++ TennisScoreboard/Data/TennisScoreboardContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TennisScoreboard.Models;
+
+namespace TennisScoreboard.Data
+{
+    public class TennisScoreboardContext(DbContextOptions<TennisScoreboardContext> options) : DbContext(options)
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Match> Matches { get; set; }
+
+    }
+}
