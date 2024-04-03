9f8aa418996aff5b46db2ca5c5d789ebfd2585d5
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-04-03 14:32:58 +0300

    Add: Controller for start new match

---
+++ TennisScoreboard/Controllers/TennisScoreboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TennisScoreboard.Contracts;
+using TennisScoreboard.Models;
+using TennisScoreboard.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
+
+namespace TennisScoreboard.Controllers
+{
+    [ApiController]
+    [Route("/new-match")]
+    public class TennisScoreboardController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult NewMatch([FromForm] NewMatchRequest matchRequest, 
+            IOngoingMatchesService ongoingMatchesService, IPlayerSerise playerSerise)
+        {
+            var player1 = playerSerise.GetPlayerByName(matchRequest.playerName1);
+            var player2 = playerSerise.GetPlayerByName(matchRequest.playerName2);
+            var guidMatch = ongoingMatchesService.StartMatch(player1, player2); // redirect
+
+            return Redirect($"/match-score?guid={guidMatch}");
+        }
+    }
+}
