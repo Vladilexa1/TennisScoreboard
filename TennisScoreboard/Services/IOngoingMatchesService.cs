4f5b6065b025991a4c63ef9af9b3c6354c517f49
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-04-03 14:32:05 +0300

    Add: interface and create new methods

---
+++ TennisScoreboard/Services/IOngoingMatchesService.cs
@@ -0,0 +1,10 @@
+﻿using TennisScoreboard.Models;
+
+namespace TennisScoreboard.Services
+{
+    public interface IOngoingMatchesService
+    {
+        Guid StartMatch(Player player1, Player player2);
+        MatchScore GetMatchForDictionary(Guid guid);
+    }
+}
--- TennisScoreboard/Services/OngoingMatchesService.cs
+++ TennisScoreboard/Services/OngoingMatchesService.cs
@@ -2,20 +2,31 @@
 
 namespace TennisScoreboard.Services
 {
-    public class OngoingMatchesService
+    public class OngoingMatchesService : IOngoingMatchesService
     {
-        public MatchScore? MatchScore { get; private set; }
-        
-        public MatchScore StartMatch()
-        {
-            MatchScore = new MatchScore();
-            MatchScore.Player1Score = new PlayerScore();
-            MatchScore.Player2Score = new PlayerScore();
-            return MatchScore;
+        private static Dictionary<Guid, MatchScore> matches = new Dictionary<Guid, MatchScore>();
+        private bool MatchIsStarted = false;
+        public Guid StartMatch(Player player1, Player player2) // see exception
+        { 
+            if (!MatchIsStarted)
+            {
+                MatchIsStarted = true;
+                var matchScore = new MatchScore();
+                matchScore.Player1Score = new PlayerScore{ Id = player1.Id};
+                matchScore.Player2Score = new PlayerScore { Id = player2.Id };
+                var guid = Guid.NewGuid();
+                matches.Add(guid, matchScore );
+                return guid;
+            }
+            else
+            {
+                throw new Exception();
+            }
         }
-        //public MatchScore GetMatchScore() // custom Exception
-        //{
-        //    return MatchScore ?? throw new Exception();
-        //}
+        public MatchScore GetMatchForDictionary(Guid guid)
+        {
+            matches.TryGetValue(guid, out MatchScore matchScore);
+            return matchScore ?? throw new ArgumentException();
+        }
     }
 }
