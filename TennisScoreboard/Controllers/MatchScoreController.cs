521ca5a3f57e033ae122d02b7399fb350524178f
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-04-03 14:34:30 +0300

    Add: controller for add score point

---
+++ TennisScoreboard/Controllers/MatchScoreController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TennisScoreboard.Contracts;
+using TennisScoreboard.Models;
+using TennisScoreboard.Services;
+
+namespace TennisScoreboard.Controllers
+{
+    [ApiController]
+    [Route("/match-score")]
+    public class MatchScoreController : ControllerBase
+    {
+        public static MatchScore currentMatchScore;
+        [HttpGet]
+        public ActionResult GetMatch([FromQuery] string guid, IOngoingMatchesService matchesService, IPlayerSerise playerSerise)
+        {
+            var guidMatch = Guid.Parse(guid);
+            currentMatchScore = matchesService.GetMatchForDictionary(guidMatch);
+            var namePlayer1 = playerSerise.GetPlayerById(currentMatchScore.Player1Score.Id).Name;
+            var namePlayer2 = playerSerise.GetPlayerById(currentMatchScore.Player2Score.Id).Name;
+            return Ok(new MatchScoreResponse(namePlayer1, namePlayer2, currentMatchScore));
+        }
+        [HttpPost("{id}")]
+        public ActionResult AddPoint(int id,
+            IOngoingMatchesService ongoingMatchesService,
+            IMatchScoreCalculationService calculationService, 
+            IFinishedMatchesPersistenceService finishedService)
+        {
+            var matchScore = calculationService.AddPointForPlayer(currentMatchScore, id);
+            currentMatchScore = matchScore;
+            if (currentMatchScore.Player1Score.Set == 2 || currentMatchScore.Player2Score.Set == 2)
+            {
+                finishedService.SaveEndedMatch(currentMatchScore);
+            }
+
+
+            return Ok(matchScore);
+        } 
+    }
+}
