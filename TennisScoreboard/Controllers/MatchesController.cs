fe4e2ca0ba6776a3031823b3aeeb6f8c0ac3e87a
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-04-07 13:37:20 +0300

    Revert "Revert "Fixed: delete current match""
    This reverts commit b240aedca16bc56a4987341d6cb74a797a7087d5.

---
+++ TennisScoreboard/Controllers/MatchesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using TennisScoreboard.Services;
+
+namespace TennisScoreboard.Controllers
+{
+    [ApiController]
+    [Route("/matches")]
+    public class MatchesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetEndedMatches(IMatchesService matches, [FromQuery] string playerName, [FromQuery] int page = 1)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ TennisScoreboard/Services/IMatchesService.cs
@@ -0,0 +1,10 @@
+﻿using TennisScoreboard.Models;
+
+namespace TennisScoreboard.Services
+{
+    public interface IMatchesService
+    {
+        public List<Match> GetAllMatch();
+        public List<Match> GetMatchByPage(int page, int pageSize);
+    }
+}
