8c103e38289411f29fc6f8227e878d145d21adba
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-05-17 14:22:11 +0300

    Add: js and css

---
+++ TennisScoreboard/wwwroot/js/JavaScript.js
@@ -0,0 +1,90 @@
+﻿const uri = 'http://localhost:5052';
+let firstGamer = document.getElementById('firstGamer');
+let secondGamer = document.getElementById('secondGamer');
+
+let table = document.getElementById('scoreTable');
+
+function addPoint(buttonValue) {
+
+    const item = {
+    };
+
+    fetch(`${uri}/match-score/${buttonValue}`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => _displayItems(data))
+}
+function _displayItems(data) {
+    const tBody = document.getElementById('table-body');
+    tBody.innerHTML = '';
+
+    const items = Object.values(data);
+
+    if (data.winnerName == undefined) {
+        items.forEach(item => {
+            let editButton;
+
+            if (item === items[0]) {
+                editButton = firstGamer.cloneNode(true);
+            } else {
+                editButton = secondGamer.cloneNode(true);
+            }
+
+            let tr = tBody.insertRow();
+
+            let td1 = tr.insertCell(0);
+            let name = document.createTextNode(item.name);
+            td1.appendChild(name);
+
+            let td2 = tr.insertCell(1);
+            let set = document.createTextNode(item.set);
+            td2.appendChild(set);
+
+            let td3 = tr.insertCell(2);
+            let game = document.createTextNode(item.game);
+            td3.appendChild(game);
+
+            let td4 = tr.insertCell(3);
+            let textNode = document.createTextNode(item.point);
+            td4.appendChild(textNode);
+
+            let td5 = tr.insertCell(4);
+            td5.appendChild(editButton);
+
+            editButton.addEventListener("click", (e) => {
+                const buttonValue = e.target.getAttribute('data-id');
+                addPoint(buttonValue, Response => {
+                    console.log(Response);
+                });
+            })
+        });
+    } else {
+        let container = document.getElementById('window-score');
+
+        container.innerHTML = '';
+        let newA = document.createElement('a');
+        newA.textContent = `Winner: ${data.winnerName}`;
+        newA.className = 'button-point'
+        newA.href = `${uri}`
+        container.appendChild(newA);
+        
+    }
+}
+
+firstGamer.addEventListener("click", (e) => {
+    const buttonValue = e.target.getAttribute('data-id');
+    addPoint(buttonValue, Response => {
+        console.log(Response);
+    });
+})
+secondGamer.addEventListener("click", (e) => {
+    const buttonValue = e.target.getAttribute('data-id');
+    addPoint(buttonValue, Response => {
+        console.log(Response);
+    });
+})
---
+++ TennisScoreboard/wwwroot/js/matches.js
@@ -0,0 +1,96 @@
+﻿const uri = 'http://localhost:5052';
+
+let playerName = '';
+
+let searchButton = document.getElementById('searchButton');
+
+let currentUrl = window.location.href;
+let urlParams = new URLSearchParams(new URL(currentUrl).search);
+let pageValue = parseInt(urlParams.get('page'));
+let previousPageTd = document.getElementById('previousPageTd');
+let nextPageTd = document.getElementById('nextPageTd');
+nextButtonWriter();
+previousButtonWriter();
+let nextPage = document.getElementById('nextPage');
+nextPage.addEventListener("click", (e) => {
+    pageValue++;
+    openPage()
+        .then(response => {
+            console.log(response);
+        });
+});
+searchButton.addEventListener("click", (e) => {
+    let inputName = document.getElementById('inputName');
+    playerName = inputName.value;
+    pageValue = 1;
+    openPage()
+        .then(response => {
+            console.log(response);
+        });
+});
+function nextButtonWriter() {
+    if (hasDataInTable()) { // если таблица меньше 7, кнопку не рисуем
+        nextPageTd.innerHTML = ''
+        nextPageTd.className = ''
+        let nextPage = document.createElement('button');
+        nextPage.className = 'button-nextPage';
+        nextPage.id = 'nextPage';
+        nextPage.textContent = '>';
+        nextPageTd.appendChild(nextPage);
+        return;
+    }
+    fetch(`${uri}/matches?playerName=${playerName}&page=${pageValue + 1}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/html'
+        }
+    }).then(Response => {
+        if (Response.status != 404) {
+            nextPageTd.innerHTML = ''
+            nextPageTd.className = ''
+            let nextPage = document.createElement('button');
+            nextPage.className = 'button-nextPage';
+            nextPage.id = 'nextPage';
+            nextPage.textContent = '>';
+            nextPageTd.appendChild(nextPage);
+        }
+    });
+}
+function previousButtonWriter() {
+    if (pageValue != 1) {
+        previousPageTd.innerHTML = ''
+        previousPageTd.className = ''
+        let previousPage = document.createElement('button');
+        previousPage.className = 'button-nextPage';
+        previousPage.id = 'previousPage';
+        previousPage.textContent = '<';
+        previousPageTd.appendChild(previousPage);
+        previousPage.addEventListener("click", (e) => {
+            pageValue--;
+            openPage()
+                .then(response => {
+                    console.log(response);
+                });
+        });
+    }
+}
+function hasDataInTable() {
+    let table = document.getElementById('table');
+    return table.rows.length == 7; // 7 i`ts header + 5 row data + row button
+}
+function openPage() {
+
+    return fetch(`${uri}/matches?playerName=${playerName}&page=${pageValue}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/html'
+        }
+    })
+        .then(response => {
+            response.json()
+            window.location.href = `${uri}/matches?playerName=${playerName}&page=${pageValue}`
+            playerName = ''
+        });
+}
