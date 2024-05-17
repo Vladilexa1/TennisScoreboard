26c82794b98c412633c7419a03c515d6ad4ac14e
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-05-17 14:17:04 +0300

    Add: returned exeprion status code, View

--- TennisScoreboard/Controllers/MatchScoreController.cs
+++ TennisScoreboard/Controllers/MatchScoreController.cs
@@ -7,19 +7,20 @@ namespace TennisScoreboard.Controllers
 {
     [ApiController]
     [Route("/match-score")]
-    public class MatchScoreController : ControllerBase
+    public class MatchScoreController : Controller
     {
         public static MatchScore currentMatchScore;
+        public static Guid guidMatch;
         [HttpGet]
         public ActionResult GetMatch([FromQuery] string guid, IOngoingMatchesService matchesService, IPlayerSerise playerSerise)
         {
-            var guidMatch = Guid.Parse(guid);
+            guidMatch = Guid.Parse(guid);
             currentMatchScore = matchesService.GetMatchForDictionary(guidMatch);
             var namePlayer1 = playerSerise.GetPlayerById(currentMatchScore.Player1Score.Id).Name;
             var namePlayer2 = playerSerise.GetPlayerById(currentMatchScore.Player2Score.Id).Name;
-            var score = new ScoreResponseBuilder(currentMatchScore);
-            
-            return Ok(new MatchScoreResponse(namePlayer1, namePlayer2, new MatchScoreContracts(score.PlayerScore1, score.PlayerScore2)));
+            var score = new ScoreResponseBuilder(currentMatchScore, namePlayer1, namePlayer2);
+
+            return View("ScoreBoard", new MatchScoreContracts(score.PlayerScore1, score.PlayerScore2));//Ok();
         }
         [HttpPost("{id}")]
         public ActionResult AddPoint(int id,
@@ -34,20 +35,13 @@ namespace TennisScoreboard.Controllers
             if (currentMatchScore.Player1Score.Set == 2 || currentMatchScore.Player2Score.Set == 2)
             {
                 currentMatchScore.WinnerId = currentMatchScore.Player1Score.Set == 2 ? currentMatchScore.Player1Score.Id : currentMatchScore.Player2Score.Id;
-                var match = finishedService.SaveEndedMatch(currentMatchScore);
+                var match = finishedService.SaveEndedMatch(currentMatchScore, guidMatch);
                 var winnerName = currentMatchScore.WinnerId == currentMatchScore.Player1Score.Id ? namePlayer1 : namePlayer2;
                 currentMatchScore = null;
                 return Ok(new MatchesResponse(match.Result.Id, namePlayer1, namePlayer2, winnerName));
             }
-           
-            var score = new ScoreResponseBuilder(currentMatchScore);
-            return Ok(new MatchScoreResponse(namePlayer1, namePlayer2, new MatchScoreContracts(score.PlayerScore1, score.PlayerScore2)));
-        }
-       
-        
-        public MatchScore test()
-        {
-            return new MatchScore { Player1Score = new PlayerScore { Id = 3, Game = 5, Point = 3, Set = 1 }, Player2Score = new PlayerScore { Id = 4, Game = 0, Point = 0, Set = 1 } };
+            var score = new ScoreResponseBuilder(currentMatchScore, namePlayer1, namePlayer2);
+            return Ok(new MatchScoreContracts(score.PlayerScore1, score.PlayerScore2));
         }
     }
 }
