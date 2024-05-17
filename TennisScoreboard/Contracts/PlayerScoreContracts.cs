9c098b04bc9dd2efe9fd7c60b396ddb63fda1fd7
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-05-17 14:15:39 +0300

    Add: field name

--- TennisScoreboard/Contracts/PlayerScoreContracts.cs
+++ TennisScoreboard/Contracts/PlayerScoreContracts.cs
@@ -3,6 +3,7 @@
     public record PlayerScoreContracts
     {
         public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Set { get; set; } = 0;
         public int Game { get; set; } = 0;
         public string Point { get; set; } = string.Empty;
